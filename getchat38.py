aee4117ebbc4cf479ef67ea89f56f506f9bdd46f
aakashravi <kshravi86@gmail.com>
2018-05-31 15:55:24 +0530

    added

--- getchat38.py
+++ getchat38.py
@@ -13,7 +13,7 @@ options = webdriver.ChromeOptions()
 options.add_argument("headless")
 
 # Google Chrome
-driver = webdriver.Chrome(chrome_options=options,executable_path='/home/aakashravi/selrandom/chromedriver3')
+driver = webdriver.Chrome(chrome_options=options,executable_path='/home/aakashravi/selrandom/chromedriver')
 
 # ------------------------------
 # The actual test scenario: Test the codepad.org code execution service.
